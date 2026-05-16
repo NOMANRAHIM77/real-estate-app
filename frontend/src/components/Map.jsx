d295fc6eef6c53002366e1b7237303a4d7292d9a
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-05-16 23:20:42 +0500

    updated map.jsx

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,15 +1,33 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import Pin from "../pin/Pin";
+import Pin from "./Pin";
 
-function Map({ items }) {
+function Map({ items = [] }) {
+
+  // Filter only valid coordinates
+  const validItems = items.filter(
+    (item) =>
+      item?.latitude &&
+      item?.longitude &&
+      !isNaN(parseFloat(item.latitude)) &&
+      !isNaN(parseFloat(item.longitude))
+  );
+
+  // Default fallback center
+  const defaultCenter = [52.4797, -1.90269];
+
+  // Dynamic center
+  const center =
+    validItems.length === 1
+      ? [
+          parseFloat(validItems[0].latitude),
+          parseFloat(validItems[0].longitude),
+        ]
+      : defaultCenter;
+
   return (
     <MapContainer
-      center={
-        items.length === 1
-          ? [items[0].latitude, items[0].longitude]
-          : [52.4797, -1.90269]
-      }
+      center={center}
       zoom={7}
       scrollWheelZoom={false}
       className="w-full h-full rounded-[20px]"
@@ -18,7 +36,8 @@ function Map({ items }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {items.map((item) => (
+
+      {validItems.map((item) => (
         <Pin item={item} key={item.id} />
       ))}
     </MapContainer>
