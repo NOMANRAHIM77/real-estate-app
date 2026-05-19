ba05855a7efd5ad853a1b861b9262e47412e2779
NOMANRAHIM77 <nomanrahim53@gmail.com>
2026-05-19 10:47:37 +0500

    list view for posts

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -1,13 +1,20 @@
+import React from "react";
 import Card from "./Card";
 
-function List({ posts }) {
+const List = ({ post }) => {
+  const safePosts = Array.isArray(post) ? post : [];
+
   return (
     <div className="flex flex-col gap-[50px]">
-      {posts.map((item) => (
-        <Card key={item.id} item={item} />
-      ))}
+      {safePosts.length > 0 ? (
+        safePosts.map((item) => (
+          <Card key={item.id} item={item} />
+        ))
+      ) : (
+        <p className="text-gray-400">No posts found</p>
+      )}
     </div>
   );
-}
+};
 
 export default List;
